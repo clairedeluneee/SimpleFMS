fc0a68d02e0b652a87828f2a082394b5add0eff4
Claire Vivian de Lunette <whotookclaireee@gmail.com>
2026-03-10 20:09:23 +0800

    actual mess

--- src/simplefms/Main.java
+++ src/simplefms/Main.java
@@ -1,15 +1,120 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package simplefms;
 
-/**
- *
- * @author CompLab209-PC1
- */
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-    
+        System.out.println("meow");
+
+        ManagementSystem ms = new ManagementSystem();
+
+        int action = -1;
+
+        Scanner s = new Scanner(System.in);
+        while (true) {
+            clear();
+
+            prints("""
+                    ============ FILE MANAGEMENT SYSTEM ==========
+                    1. Create a Text File and Write Content
+                    2. Convert File Path to Absolute Path
+                    3. Display File Path Information (Name Count)
+                    4. Check File Read and Execute Permission
+                    5. Display File Attributes
+                    6. Read a Text File
+                    7. Exit
+                    Enter your choice:
+                    """);
+            try {
+
+                action = s.nextInt();
+                
+            } catch (Exception e) {
+                continue;
+            }
+
+            if (action < 1) continue;
+            if (action > 7) continue;
+
+            // i love reusing code
+            switch (action) {
+                case 1:
+                    ms.doUserWrite();
+                    break;
+                
+                case 2:
+                    prints("Filename: ");
+                    String fileToGetAbsolutePathFrom = s.nextLine();
+
+                    try {
+                        print(ms.getAbsolutePathFromRelativeFile(fileToGetAbsolutePathFrom));
+                    } catch (Exception e) {
+                        print("file not found");
+                    }
+                    break;
+                
+                case 3:
+                    prints("Filename: ");
+                    String fileToGetPathInformationFrom = s.nextLine();
+
+                    try {
+                        ms.displayFileAttributes(Paths.get(fileToGetPathInformationFrom));
+                    } catch (Exception e) {
+                        print("file not found");
+                    }
+                    break;
+
+                case 4:
+                    prints("Filename: ");
+                    String fileToGetPermissionsFrom = s.nextLine();
+
+                    try {
+                        ms.displayFilePermissions(fileToGetPermissionsFrom);
+                    } catch (Exception e) {
+                        print("file not found");
+                    }
+                    break;
+                
+                case 5:
+                    prints("Filename: ");
+                    String fileToGetAttributesFrom = s.nextLine();
+
+                    try {
+                        ms.displayFileAttributes(Paths.get(fileToGetAttributesFrom));
+                    } catch (Exception e) {
+                        print("file not found");
+                    }
+                    break;
+                
+                case 6:
+                    ms.doUserRead();
+                    break;
+
+                case 7:
+                    ms.exit();
+
+                default:
+                    continue;
+            }
+
+
+            if (!ms.isActive()) break;
+
+            print("\nPress enter to continue");
+            @SuppressWarnings("unused")
+            String thisIsHereSoThatTheUserHasToPressEnterToRefreshTheThing = s.nextLine();
+        }
     } 
+
+    public static void clear() {
+        System.out.print("\u001b[2J");
+        System.out.flush();
+    }
+    private static void print(String message) {
+        System.out.println(message + "");
+    }
+    private static void prints(String message) {
+        System.out.print(message + "");
+    }
 }
--- src/simplefms/ManagementSystem.java
+++ src/simplefms/ManagementSystem.java
@@ -1,20 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package simplefms;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.*;
 import static java.nio.file.AccessMode.*;
-
-import java.io.*;
-import java.io.IOException;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Scanner;
 
 /**
- * The management system itself. This is only the backend, and interfaces with ClefFrontend for the terminal user interface.
+ * The management system itself. 
  */
 public class ManagementSystem {
     private FileSystem fs;
+
+    private boolean loopActive = true;
+    public boolean isActive() { return this.loopActive;}
     
 
     // Constructors
@@ -53,29 +56,98 @@ public class ManagementSystem {
         // read check
         try {
             provider.checkAccess(path, READ);
-            output += 1;
+            output += 1; // 001
         } catch (IOException e) {
         }
 
         // write check
         try {
             provider.checkAccess(path, WRITE);
+            output += 2; // 010
         } catch (IOException e) {
         }
 
         // execute check
         try {
             provider.checkAccess(path, EXECUTE);
-        } catch (Exception e) {
+            output += 4; // 100
+        } catch (IOException e) {
         }
 
         return output;
     }
 
+    public void displayFilePermissions(String file) {
+        print("File permissions for " + file);
 
+        byte code = (byte) checkFilePermissions(file);
+        
+        // god i wish there was an easier way to do this
+        if (code == 0) print("X Readable\nX Writable\nX Executable");
+        if (code == 1) print("O Readable\nX Writable\nX Executable");
+        if (code == 2) print("X Readable\nO Writable\nX Executable");
+        if (code == 3) print("O Readable\nO Writable\nX Executable");
+        if (code == 4) print("X Readable\nX Writable\nO Executable");
+        if (code == 5) print("O Readable\nX Writable\nO Executable");
+        if (code == 6) print("X Readable\nO Writable\nO Executable");
+        if (code == 7) print("O Readable\nO Writable\nO Executable");
+    }
+
+    public void displayFileAttributes(Path filepath) {
+        print("File attributes for " + filepath.toString());
+
+        try {
+            BasicFileAttributes bfa = Files.readAttributes(filepath, BasicFileAttributes.class);
+
+            print("Created at:    " + bfa.creationTime());
+            print("Last modified: " + bfa.lastModifiedTime());
+            print("Size:          " + bfa.size() + " byte" + (bfa.size() == 1 ? "" : "s"));
+        } catch (IOException e) {
+
+        }
+    }
+
+    public void doUserWrite() {
+        Scanner s = new Scanner(System.in);
+        try {
+            prints("Enter filename: ");
+            String outputFilename = s.nextLine();
+
+            BufferedWriter bw = new BufferedWriter(new FileWriter(outputFilename));
+
+            print("\nEnter file content:");
+            bw.write(s.nextLine());
+        } catch (IOException e) {
+
+        }
+    }
+
+    public void doUserRead() {
+        Scanner s = new Scanner(System.in);
+        try {
+            prints("Enter filename: ");
+            String inputFilename = s.nextLine();
+
+            BufferedReader br = new BufferedReader(new FileReader(inputFilename));
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                print(line);
+            }
+        } catch (IOException e) {
+
+        }
+    }
+
+    public void exit() {
+        loopActive = false;
+    }
 
     // Boilerplate code.
     private void print(String message) {
         System.out.println(message + "");
+    }
+    private void prints(String message) {
+        System.out.print(message + "");
     }
 }
